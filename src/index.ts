177801ab844f9f0edabe93de2c3abd6102780337
Michal Mitter <mittermichal@gmail.com>
2019-04-24 14:04:14 +0200

    webpack - fabric external not working

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {a} from "./hud"
+import { fabric } from 'fabric'
+// import Vue from 'vue'
+//import { Canvas } from "fabric/fabric-impl";
+
+// var test = new Vue();
+//var fabric = require('fabric');
+console.log(a);
+let canvas = new fabric.Canvas('c');
+
+/* var rect = new fabric.Circle({
+    left: 100,
+    radius: 50,
+    top: 100
+}); */
+
