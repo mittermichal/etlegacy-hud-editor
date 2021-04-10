05a9d1f4701f3cbe018af5526035692e3701eb2e
Michal Mitter <mittermichal@gmail.com>
2021-04-10 23:30:18 +0200

    no idea, changes made too long ago :)

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ class Hud {
 		let ret = "hud {\n\thudnumber "+ this.hudNumber.toString() + "\n";
 		this.elements.forEach(function (el) {
 			// console.log(el);
-			ret += '\t' + el.toString() + '\n';
+			ret += '\t\t' + el.toString() + '\n';
 		});
 		return ret + "}";
 	}
@@ -70,7 +70,7 @@ class HudElement {
 // so 1. implement string to huddef
 // 2. implement hud obj to string
 
-const SCREEN_WIDTH = 640;
+const SCREEN_WIDTH = 640; // 16/9: 853.333333
 const SCREEN_HEIGHT = 480;
 const STYLE_NORMAL = componentStyle.STYLE_NORMAL;
 const STYLE_SIMPLE = componentStyle.STYLE_SIMPLE;
@@ -81,6 +81,10 @@ const SKILL_ICON_X = (SKILL_BAR_OFFSET + SKILLS_X);
 const qtrue = 1;
 const qfalse = 0;
 
+const CHATLOC_Y = 478;
+const TEAMCHAT_HEIGHT = 4;
+const TEAMCHAT_LINE_HEIGHT = 9;
+
 /*
 float Ccg_WideX(float x)
 {
@@ -88,14 +92,22 @@ float Ccg_WideX(float x)
 }
 */
 
-function Ccg_Is43Screen() {return true;} //TODO
-var cgs = { adr43: 0 }; //TODO
+const RATIO43   = (4.0 / 3.0);   ///< 4:3 aspectratio is the default for this game engine ...
+const RPRATIO43 = (1.0 / RATIO43);
+const windowAspect = 1.0;
+function Ccg_Is43Screen() {return false;} //TODO
+var cgs = { adr43: windowAspect * RPRATIO43 }; //TODO
 
 function Ccg_WideX(x: number) {
 	return (Ccg_Is43Screen()) ? x : x *cgs.adr43; // * (aspectratio / (4/3))
 }
 
-function CG_getComponent(...argv: any) {return argv;}
+function CG_getComponent(...argv: any) {
+	var tmp = argv[4];
+	argv[4] = argv[5];
+	argv[5] = tmp;
+	return argv;
+}
 
 
 
@@ -107,7 +119,7 @@ var hud0 = {
 	staminabar:	CG_getComponent(4, SCREEN_HEIGHT - 92, 12, 72, qtrue, STYLE_NORMAL),
 	breathbar:	CG_getComponent(4, SCREEN_HEIGHT - 92, 12, 72, qtrue, STYLE_NORMAL),
 	healthbar:	CG_getComponent(24, SCREEN_HEIGHT - 92, 12, 72, qtrue, STYLE_NORMAL),
-	weaponchargebar:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 16, SCREEN_HEIGHT - 92, 12, 72, qtrue, STYLE_NORMAL),
+	weaponchangebar:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 16, SCREEN_HEIGHT - 92, 12, 72, qtrue, STYLE_NORMAL),
 	healthtext:	CG_getComponent(SKILLS_X - 28, SCREEN_HEIGHT - 4, 0, 0, qtrue, STYLE_NORMAL),
 	xptext:	CG_getComponent(SKILLS_X + 28, SCREEN_HEIGHT - 4, 0, 0, qtrue, STYLE_NORMAL),
 	statsdisplay:	CG_getComponent(SKILL_ICON_X, 0, 0, 0, qtrue, STYLE_NORMAL),
@@ -117,7 +129,7 @@ var hud0 = {
 	popupmessages:	CG_getComponent(4, 360, 72, 72, qtrue, STYLE_NORMAL),
 	powerups:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 40, SCREEN_HEIGHT - 140, 36, 36, qtrue, STYLE_NORMAL),
 	hudhead:	CG_getComponent(44, SCREEN_HEIGHT - 92, 62, 80, qtrue, STYLE_NORMAL),
-	cursorhint:	CG_getComponent(.5 * SCREEN_WIDTH - .5 * 48, 260, 48, 48, qtrue, STYLE_NORMAL), // FIXME: widescreen ?
+	cursorhints:	CG_getComponent(.5 * SCREEN_WIDTH - .5 * 48, 260, 48, 48, qtrue, STYLE_NORMAL), // FIXME: widescreen ?
 	weaponstability:	CG_getComponent(50, 208, 10, 64, qtrue, STYLE_NORMAL),
 	livesleft:	CG_getComponent(0, 0, 0, 0, qtrue, STYLE_NORMAL),
 	reinforcment:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 70, SCREEN_HEIGHT - 70, 0, 0, qtrue, STYLE_NORMAL),
@@ -132,7 +144,7 @@ var hud1 = {
 	staminabar:	CG_getComponent(4, 388, 12, 72, qtrue, STYLE_NORMAL),
 	breathbar:	CG_getComponent(4, 388, 12, 72, qtrue, STYLE_NORMAL),
 	healthbar:	CG_getComponent((Ccg_WideX(SCREEN_WIDTH) - 36), 388, 12, 72, qtrue, STYLE_NORMAL),
-	weaponchargebar:	CG_getComponent((Ccg_WideX(SCREEN_WIDTH) - 16), 388, 12, 72, qtrue, STYLE_NORMAL),
+	weaponchangebar:	CG_getComponent((Ccg_WideX(SCREEN_WIDTH) - 16), 388, 12, 72, qtrue, STYLE_NORMAL),
 	healthtext:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 60, SCREEN_HEIGHT - 65, 0, 0, qtrue, STYLE_NORMAL),
 	xptext:	CG_getComponent(48, SCREEN_HEIGHT - 4, 0, 0, qtrue, STYLE_NORMAL),
 	statsdisplay:	CG_getComponent(24, SCREEN_HEIGHT - 95, 0, 0, qtrue, STYLE_SIMPLE),
@@ -142,7 +154,7 @@ var hud1 = {
 	popupmessages:	CG_getComponent(4, 100, 72, 72, qtrue, STYLE_NORMAL),
 	powerups:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 40, SCREEN_HEIGHT - 140, 36, 36, qtrue, STYLE_NORMAL),
 	hudhead:	CG_getComponent(44, SCREEN_HEIGHT - 92, 62, 80, qfalse, STYLE_NORMAL),
-	cursorhint:	CG_getComponent(.5 * SCREEN_WIDTH - .5 * 48, 260, 48, 48, qtrue, STYLE_NORMAL),
+	cursorhints:	CG_getComponent(.5 * SCREEN_WIDTH - .5 * 48, 260, 48, 48, qtrue, STYLE_NORMAL),
 	weaponstability:	CG_getComponent(50, 208, 10, 64, qtrue, STYLE_NORMAL),
 	livesleft:	CG_getComponent(0, 0, 0, 0, qtrue, STYLE_NORMAL),
 	reinforcment:	CG_getComponent(70, SCREEN_HEIGHT - 12, 0, 0, qtrue, STYLE_NORMAL),
@@ -157,7 +169,7 @@ var hud2 = {
 	staminabar:	CG_getComponent(4, 388, 12, 72, qtrue, STYLE_NORMAL),
 	breathbar:	CG_getComponent(4, 388, 12, 72, qtrue, STYLE_NORMAL),
 	healthbar:	CG_getComponent(24, 388, 12, 72, qtrue, STYLE_NORMAL),
-	weaponchargebar:	CG_getComponent((Ccg_WideX(SCREEN_WIDTH) - 16), 388, 12, 72, qtrue, STYLE_NORMAL),
+	weaponchangebar:	CG_getComponent((Ccg_WideX(SCREEN_WIDTH) - 16), 388, 12, 72, qtrue, STYLE_NORMAL),
 	healthtext:	CG_getComponent(65, SCREEN_HEIGHT - 4, 0, 0, qtrue, STYLE_NORMAL),
 	xptext:	CG_getComponent(120, SCREEN_HEIGHT - 4, 0, 0, qtrue, STYLE_NORMAL),
 	statsdisplay:	CG_getComponent(44, SCREEN_HEIGHT - 95, 0, 0, qtrue, STYLE_SIMPLE),
@@ -167,7 +179,46 @@ var hud2 = {
 	popupmessages:	CG_getComponent(4, 100, 72, 72, qtrue, STYLE_NORMAL),
 	powerups:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 40, SCREEN_HEIGHT - 140, 36, 36, qtrue, STYLE_NORMAL),
 	hudhead:	CG_getComponent(44, SCREEN_HEIGHT - 92, 62, 80, qfalse, STYLE_NORMAL),
-	cursorhint:	CG_getComponent(.5 * SCREEN_WIDTH - .5 * 48, 260, 48, 48, qtrue, STYLE_NORMAL),
+	cursorhints:	CG_getComponent(.5 * SCREEN_WIDTH - .5 * 48, 260, 48, 48, qtrue, STYLE_NORMAL),
+	weaponstability:	CG_getComponent(50, 208, 10, 64, qtrue, STYLE_NORMAL),
+	livesleft:	CG_getComponent(0, 0, 0, 0, qtrue, STYLE_NORMAL),
+	reinforcment:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 70, SCREEN_HEIGHT - 70, 0, 0, qtrue, STYLE_NORMAL),
+	roundtimer:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 55, SCREEN_HEIGHT - 70, 0, 0, qtrue, STYLE_NORMAL),
+	spawntimer:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 70, SCREEN_HEIGHT - 60, 0, 0, qtrue, STYLE_NORMAL),
+	localtime:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 55, SCREEN_HEIGHT - 60, 0, 0, qtrue, STYLE_NORMAL),
+};
+
+const COMPASS_SIZE = 132;
+const HEAD_SIZE = 12;
+const WEAPON_ICON_WIDTH = 30;
+const WEAPON_ICON_HEIGHT = (WEAPON_ICON_WIDTH * 32) / 60.0;
+const BOTTOM_PADDING = 73.77777777777777;
+const BARS_HEIGHT = 72;
+const BARS_BOTTOM_ICON = 12;
+const LEFT_COMPASS = .5 * SCREEN_WIDTH - Ccg_WideX(.5 * COMPASS_SIZE) + 3;
+const TEAMCHAT_TOP = CHATLOC_Y - TEAMCHAT_LINE_HEIGHT * TEAMCHAT_HEIGHT;
+
+const chatWidth   = 320;
+const chatPosX    = (Ccg_WideX(SCREEN_WIDTH) - chatWidth) / 2.0;
+
+
+var hud3 = {
+	hudnumber:	5,
+	compas:	CG_getComponent(.5 * SCREEN_WIDTH - Ccg_WideX(.5 * COMPASS_SIZE),  TEAMCHAT_TOP - COMPASS_SIZE, COMPASS_SIZE, COMPASS_SIZE, qtrue, STYLE_NORMAL),
+	staminabar:	CG_getComponent(LEFT_COMPASS - Ccg_WideX(12 + 12), SCREEN_HEIGHT - BOTTOM_PADDING - BARS_HEIGHT - BARS_BOTTOM_ICON, 12, BARS_HEIGHT, qtrue, STYLE_NORMAL),
+	breathbar:	CG_getComponent(LEFT_COMPASS - Ccg_WideX(12 + 12), SCREEN_HEIGHT - BOTTOM_PADDING - BARS_HEIGHT - BARS_BOTTOM_ICON, 12, BARS_HEIGHT, qtrue, STYLE_NORMAL),
+	healthbar:	CG_getComponent(LEFT_COMPASS - Ccg_WideX(12), SCREEN_HEIGHT - BOTTOM_PADDING - BARS_HEIGHT - BARS_BOTTOM_ICON, 12, BARS_HEIGHT, qtrue, STYLE_NORMAL),
+	weaponchangebar:	CG_getComponent((Ccg_WideX(SCREEN_WIDTH) - 16), SCREEN_HEIGHT + BOTTOM_PADDING - BARS_HEIGHT - BARS_BOTTOM_ICON, 12, BARS_HEIGHT, qtrue, STYLE_NORMAL),
+	healthtext:	CG_getComponent(65, SCREEN_HEIGHT - BOTTOM_PADDING, 0, 0, qtrue, STYLE_NORMAL),
+	xptext:	CG_getComponent(.5 * SCREEN_WIDTH - Ccg_WideX(.5 * COMPASS_SIZE), SCREEN_HEIGHT - BOTTOM_PADDING, 0, 0, qtrue, STYLE_NORMAL),
+	statsdisplay:	CG_getComponent(.5 * SCREEN_WIDTH - Ccg_WideX(.5 * 16), SCREEN_HEIGHT - COMPASS_SIZE - 21, 0, 0, qtrue, STYLE_SIMPLE),
+	weaponicon:	CG_getComponent(.5 * SCREEN_WIDTH + Ccg_WideX(.5 * 16), SCREEN_HEIGHT - COMPASS_SIZE - 19, WEAPON_ICON_WIDTH, WEAPON_ICON_HEIGHT, qtrue, STYLE_NORMAL),
+	weaponammo:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 22, SCREEN_HEIGHT - 1 * (16 + 2) + 12 - 4, 0, 0, qtrue, STYLE_NORMAL),
+	fireteam:	CG_getComponent(10, 10, 100, 100, qtrue, STYLE_NORMAL),
+	popupmessages:	CG_getComponent(4, 100, 72, 72, qtrue, STYLE_NORMAL),
+	powerups:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 40, SCREEN_HEIGHT - 140, 36, 36, qtrue, STYLE_NORMAL),
+	hudhead:	CG_getComponent(4 + Ccg_WideX(24), 388 + 72 + 1, HEAD_SIZE, HEAD_SIZE * (80 / 62), qtrue, STYLE_NORMAL),
+	cursorhints:	CG_getComponent(.5 * SCREEN_WIDTH - .5 * 48, 260, 48, 48, qtrue, STYLE_NORMAL),
 	weaponstability:	CG_getComponent(50, 208, 10, 64, qtrue, STYLE_NORMAL),
 	livesleft:	CG_getComponent(0, 0, 0, 0, qtrue, STYLE_NORMAL),
 	reinforcment:	CG_getComponent(Ccg_WideX(SCREEN_WIDTH) - 70, SCREEN_HEIGHT - 70, 0, 0, qtrue, STYLE_NORMAL),
@@ -184,20 +235,24 @@ var hudDef = {
 		Hud.fromObj(hud0),
 		Hud.fromObj(hud1),
 		Hud.fromObj(hud2),
+		Hud.fromObj(hud3),
 	],
 	toString: function (){
 		return 'hudDef {\n\t' + this.hud[selectedHudIndex] + '\n}';
 	}
 };
 
-var selectedHudIndex = 0; 
+var selectedHudIndex = 3; 
+declare var canvas: fabric.Canvas;
+var info = "test";
 
 new Vue({
   el: '#app',
   data: {
     hudDef: hudDef, //import
 		hudExport: hudDef.hud[selectedHudIndex],
-		selectedHudIndex: selectedHudIndex
+		selectedHudIndex: selectedHudIndex,
+		info: info
 	},
 	methods: {
 		hudSelected() {
@@ -207,12 +262,21 @@ new Vue({
 		}
 	},
 	mounted() {
-		var canvas = new fabric.Canvas('c');
+		canvas = new fabric.Canvas('c');
 		drawHud(canvas,hudDef.hud[selectedHudIndex]);
 	}
 	
 })
+canvas.on({
+	'object:modified': updateControls,
+	'selection:updated': updateControls,
+	'selection:created': updateControls
+});
 
+function updateControls(e) {
+	//console.log(e.target);
+	document.getElementById('info').innerHTML = "x:" + e.target.left + "\ny:" +e.target.top + "\nw:" + e.target.width + "\nh:" + e.target.height;
+}
 
 // create a wrapper around native canvas element (with id="c")
 
